092c1098f76b4e5cf369c0e0b09d76dfcbba0320
ssnell227 <ssnell227@gmail.com>
2020-06-16 15:39:23 -0600

    getPlaylistItems works

--- server/controllers/spotifyControl.js
+++ server/controllers/spotifyControl.js
@@ -7,16 +7,24 @@ module.exports = {
         const {playlistId} = req.body
         const accessToken = await authenticate()
         .catch(err =>  console.log(err))
-        console.log(accessToken)
-        const playlist = await axios.get(`https://api.spotify.com/v1/playlists/${playlistId}`, {headers:{Authorization: 'Bearer '+ accessToken}})
+        const playlist = await axios.get(`https://api.spotify.com/v1/playlists/${playlistId}?fields=id,name,images`, {headers:{Authorization: 'Bearer '+ accessToken}})
         .catch(err => console.log(err))
-        console.log(playlist)
-
+        
         const {id, name, images} = playlist.data
 
         res.status(200).send({id, name, images})
     },
     getPlaylistItems: async (req, res) => {
-        //get items from specified playlists and return song name, artist name, album name, album art, and link to preview
+        const {playlistId} = req.body
+        const accessToken = await authenticate()
+        .catch(err =>  console.log(err))
+        const items = await axios.get(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?fields=items(track(album(name,images),artists,name,preview_url))`, {headers:{Authorization: 'Bearer '+ accessToken}})
+        .catch(err => console.log(err))
+        console.log(items)
+
+        const tracks = items.data.items
+
+        console.log(items)
+        res.status(200).send(tracks)
     }
 }
